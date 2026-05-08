cb6638e30a9f0785a105ecd0aed6c291f2ef15b3
Paul Wagner <hydrofin@gmail.com>
2026-05-08 21:01:39 +0200

    Revert "try out envtest"
    This reverts commit 9bf5193ca89f0a74651617ec75a75808d6e9a703.

--- passage-adapters/agones/tests/common/mod.rs
+++ passage-adapters/agones/tests/common/mod.rs
@@ -1,7 +1,13 @@
-use envtest::{Environment, Server};
-use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
-use kube::api::Api;
-use kube::Client;
+use futures_util::StreamExt;
+use k8s_openapi::api::core::v1::Namespace;
+use kube::api::PostParams;
+use kube::config::Kubeconfig;
+use kube::{Api, Client, Config};
+use std::env::temp_dir;
+use testcontainers::core::ExecCommand;
+use testcontainers::runners::AsyncRunner;
+use testcontainers::{ContainerAsync, ImageExt};
+use testcontainers_modules::k3s::{K3s, KUBE_SECURE_PORT};
 
 // TODO download file at compile time or add to sources
 const AGONES_INSTALL_URL: &str = "https://raw.githubusercontent.com/googleforgames/agones/release-1.57.0/install/yaml/install.yaml";
@@ -11,27 +17,136 @@ const INSTALL_YAML: &str = include_str!("install.yaml");
 const AGONES_EXAMPLE_GAMESERVER: &str = "https://raw.githubusercontent.com/googleforgames/agones/release-1.57.0/examples/simple-game-server/gameserver.yaml";
 
 pub struct K3sContainer {
-    pub server: Server,
+    pub instance: ContainerAsync<K3s>,
     pub client: Client,
 }
 
 impl K3sContainer {
     pub async fn start() -> Self {
-        // Create the envtest suite
-        let mut env = Environment::default();
-        env.crd_install_options.paths.push("/home/hydrofin/Projects/JustChunks/passage/passage-adapters/agones/tests/common/install.yaml".to_string());
-            //.with_crds(INSTALL_YAML).expect("Failed to install agones CRD");
-        let server = env.create().await.expect("Failed to create kube server");
-        let client = server.client().expect("Failed to create kube client");
+        // Create the K3s container.
+        let instance = K3s::default()
+            .with_conf_mount(temp_dir())
+            .with_privileged(true)
+            .with_userns_mode("host")
+            .start()
+            .await
+            .expect("Failed to start K3s container.");
 
-        // List all installed CRDs
-        let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
-        let crd_list = crds.list(&Default::default()).await.expect("Failed to list CRDs");
-        println!("Installed CRDs:");
-        for crd in crd_list.items {
-            println!("  - {}", crd.metadata.name.unwrap_or_default());
+        // Get the container configuration.
+        let kube_port = instance
+            .get_host_port_ipv4(KUBE_SECURE_PORT)
+            .await
+            .expect("Failed to read kube port.");
+        let kube_conf_str = instance
+            .image()
+            .read_kube_config()
+            .expect("Failed to read kubeconfig file.")
+            .replace(&format!(":{}", KUBE_SECURE_PORT.as_u16()), &format!(":{kube_port}"));
+        let kube_conf = Kubeconfig::from_yaml(&kube_conf_str)
+            .expect("Failed to parse kube config");
+        let config = Config::from_custom_kubeconfig(kube_conf, &Default::default())
+            .await
+            .expect("Failed to create Kubernetes client.");
+
+        // Install all required CRDs.
+        let client = Client::try_from(config.clone()).expect("Failed to create Kubernetes client.");
+        instance
+            .exec(ExecCommand::new(["kubectl", "create", "namespace", "agones-system"]))
+            .await
+            .expect("Failed to install Agones CRDs");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=jsonpath={.status.phase}=Active",
+                "--timeout=120s", "namespace/agones-system"
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+        println!("Agones namespace created.");
+
+        instance
+            .exec(ExecCommand::new(["kubectl", "apply", "--server-side", "-f", AGONES_INSTALL_URL]))
+            .await
+            .expect("Failed to install Agones CRDs");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=condition=available",
+                "--timeout=120s", "deployment/agones-controller",
+                "-n", "agones-system",
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=condition=established",
+                "--timeout=120s", "crd/gameservers.agones.dev"
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=condition=established",
+                "--timeout=120s", "crd/fleets.agones.dev"
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=condition=established",
+                "--timeout=120s", "crd/gameserversets.agones.dev"
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=jsonpath='{.subsets[*].addresses[0].ip}'",
+                "endpoints/agones-controller-service",
+                "-n", "agones-system",
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+
+        // Create a gameserver to test with.
+        let status = instance
+            .exec(ExecCommand::new(["kubectl", "create", "-f", AGONES_EXAMPLE_GAMESERVER]))
+            .await
+            .expect("Failed to create Agones gameserver")
+            .exit_code()
+            .await
+            .expect("Failed to create Agones gameserver");
+        println!("status {:?}", status);
+        instance
+            .exec(ExecCommand::new([
+                "kubectl", "wait", "--for=jsonpath={.status.state}=Ready",
+                "--timeout=120s", "gameserver", "--all"
+            ]))
+            .await
+            .expect("Failed to wait for gameserver to be ready");
+
+        println!("Agones example gameserver created.");
+
+        tokio::time::sleep(std::time::Duration::from_hours(10)).await;
+
+        K3sContainer { instance, client }
+    }
+}
+
+async fn create_namespace(client: Client, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let api: Api<Namespace> = Api::all(client.clone());
+    let namespace = Namespace {
+        metadata: kube::api::ObjectMeta {
+            name: Some(name.to_string()),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    match api.create(&PostParams::default(), &namespace).await {
+        Ok(_) => {
+            println!("Namespace '{}' creation initiated.", name);
+            Ok(())
+        },
+        Err(kube::Error::Api(e)) if e.code == 409 => {
+            println!("Namespace '{}' already exists, proceeding to wait for readiness.", name);
         }
-
-        Self { server, client }
+        Err(e) => Err(e.into()),
     }
 }
